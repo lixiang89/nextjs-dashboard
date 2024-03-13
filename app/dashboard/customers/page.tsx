f0e2b08b2228b6c541aefc3714c55b98f289e631
lixiang <lixiang89@163.com>
2024-03-13 18:46:54 +0800

    完成

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <p>Customers  Page</p>;
-  }
+import { fetchCustomers,fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers=await fetchFilteredCustomers(query);
+  return <Table customers={customers} />
+}
---
+++ app/dashboard/invoices/edit/[id]/page.tsx
@@ -0,0 +1,34 @@
+// 路径改成 edit/【id】，容易看到路由，也更符合~em……的习惯
+
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  console.log('Page ~ invoice:', invoice)
+  if(!invoice){
+    notFound()
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
